0c202f3bcce77d5e49162b1aa0a4940bcaed1075
adarsh yadav <ay178568@gmail.com>
2026-02-07 23:08:40 +0530

    first commit

---
+++ src/Main.kt
@@ -0,0 +1,18 @@
+fun main() {
+//    var can be reassigned.
+    var a =10
+    var b="Adarsh Yadav"
+    println(a)
+    a=120
+    println("a after reassigning : "+a)
+    println(b)
+    b="b after reassigning value"
+    println(b)
+
+    val c=10
+    val d="Hello"
+    println(c)
+    println(d)
+//    val can not be reassigned.
+//    c=14
+}
