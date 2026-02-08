fb279eb760da62a3213c9752058852441f72b487
adarsh yadav <ay178568@gmail.com>
2026-02-08 15:56:26 +0530

    function type definition

--- src/functionEx.kt
+++ src/functionEx.kt
@@ -4,6 +4,8 @@ fun main(){
     val add=addition(a,b)
     print(add)
     println("subtraction : ${sub(a,b)}")
+    greet()
+    namegreet("Adarsh yadav")
 }
 //fun addition(a1: datatype, a2 datatype): function return type
 fun addition(a1: Int,a2:Int): Int{
@@ -12,3 +14,13 @@ fun addition(a1: Int,a2:Int): Int{
 fun sub(a1: Int,a2:Int): Int{
     return a1-a2
 }
+//no param no return
+fun greet(){
+    print("Hello Duniya")
+}
+//function with parameter
+fun namegreet(name: String){
+    print("Hello : $name")
+}
+// short form of expression function
+fun mul(a1:Int,a2: Int):Int=a1*a2
