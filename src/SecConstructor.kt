cf393996d4c7b3204f7e5756f5aa0b76dd63f4ca
adarsh yadav <ay178568@gmail.com>
2026-02-10 21:23:39 +0530

    learning secondry constructor

--- src/SecConstructor.kt
+++ src/SecConstructor.kt
@@ -1,2 +1,15 @@
-class SecConstructor {
+fun main(){
+    var laptopdetail=Laptop("MacBook",98700)
+}
+class Laptop(brand: String) {
+    var price = 0
+
+    constructor(laptopbrand: String, price: Int) : this(laptopbrand) {
+        this.price = price
+        println("Full details initialized")
+
+    }
+    init {
+        println("Laptop brand initialized")
+    }
 }
