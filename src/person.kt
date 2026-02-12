833747f9aa56aa79d184155c626476dc4b817416
adarsh yadav <ay178568@gmail.com>
2026-02-12 17:57:37 +0530

    learning getter and setter

--- src/person.kt
+++ src/person.kt
@@ -1,13 +1,13 @@
 fun main(){
    var a= Person3()
-    a.age=-10
-    println(a.age)
+    a.age=-10 // internally called setter method.
+    println(a.age) // internally called getter method
 
 }
 class Person3 {
-    var age: Int=0
+    var age: Int=0 //field
         set(value) {
-            if (value<0){
+            if (value<0){ // check condition by passed value
                 field=0
             }else{
                 field=value
