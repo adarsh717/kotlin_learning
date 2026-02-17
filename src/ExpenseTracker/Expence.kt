b5ae86e7b1b505906c02f3ff689e5afb80b13d6c
adarsh yadav <ay178568@gmail.com>
2026-02-17 22:19:30 +0530

    Expence tracker

---
+++ src/ExpenseTracker/Expence.kt
@@ -0,0 +1,53 @@
+package ExpenseTracker
+
+fun main(){
+    val em= ExpenseManager()
+    em.addExpenses(1,"Salary",5000.60, TransactionType.INCOME)
+    em.addExpenses(2,"Food",1500.80, TransactionType.EXPENSE)
+    em.addExpenses(3,"Cloths",575.50, TransactionType.EXPENSE)
+    em.addExpenses(4,"Freelance",2500.0, TransactionType.INCOME)
+    println("Total Income: ${em.getTotalIncome()}")
+    println("Total Expense: ${em.getTotalExpense()}")
+}
+
+data class Expense(
+    var id: Int,
+    var title: String,
+    var amount: Double,
+    val type: TransactionType
+    )
+
+enum class TransactionType{
+    INCOME,
+    EXPENSE
+}
+
+open class ExpenseManager{
+    val transaction=mutableListOf<Expense>()
+
+    fun addExpenses(id: Int,title: String,amount: Double,type: TransactionType){
+        transaction.add(Expense(id,title,amount,type))
+    }
+
+    fun getTotalIncome(): Double{
+        var total=0.0
+
+        for (expense in transaction){
+            if (expense.type== TransactionType.INCOME){
+                total+=expense.amount
+            }
+        }
+        return total
+    }
+
+    fun getTotalExpense(): Double{
+        var total=0.0
+        for (expense in transaction){
+            if (expense.type== TransactionType.EXPENSE){
+                total+= expense.amount
+            }
+        }
+        return total
+    }
+
+}
